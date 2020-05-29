22593048f7a32e603e67845c68d0371619ba81f9
lincidro <lincidro@gmail.com>
2020-05-29 00:15:57 -0500

    Agregando el state

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Formulario = (props) => {
+  const mascota = {
+    nombre: '',
+    propietario: '',
+    fecha: '',
+    hora: '',
+    comentarios: '',
+  };
+
+  const [cita, actualizarCita] = useState(mascota);
+
+  const handleChange = () => {
+    console.log('handleChange');
+  };
+
   return (
     <Fragment>
       <h2>Crear Cita</h2>
@@ -12,6 +26,7 @@ const Formulario = (props) => {
           name='nombre'
           className='u-full-width'
           placeholder='Nombre mascota'
+          onChange={handleChange}
         />
 
         <label htmlFor='propietario'>Dueño</label>
@@ -21,16 +36,34 @@ const Formulario = (props) => {
           name='propietario'
           className='u-full-width'
           placeholder='Nombre del dueño'
+          onChange={handleChange}
         />
 
         <label htmlFor='fecha'>Fecha</label>
-        <input type='date' id='fecha' name='fecha' className='u-full-width' />
+        <input
+          type='date'
+          id='fecha'
+          name='fecha'
+          className='u-full-width'
+          onChange={handleChange}
+        />
 
         <label htmlFor='hora'>Hora</label>
-        <input type='time' id='hora' name='hora' className='u-full-width' />
+        <input
+          type='time'
+          id='hora'
+          name='hora'
+          className='u-full-width'
+          onChange={handleChange}
+        />
 
         <label htmlFor='comentarios'>Comentarios</label>
-        <textarea name='comentarios' id='comentarios' className='u-full-width'></textarea>
+        <textarea
+          name='comentarios'
+          id='comentarios'
+          className='u-full-width'
+          onChange={handleChange}
+        ></textarea>
 
         <button type='submit' className='u-full-width button-primary'>
           Agregar cita
