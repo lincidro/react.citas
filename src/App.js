be34dccdf20d52374b60282b8d77dba05e2c604f
lincidro <lincidro@gmail.com>
2020-05-30 19:53:49 -0500

    Se agrega useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
 function App() {
   //Arreglo de citas
   const [citas, setCitas] = useState([]);
+
+  //useEffect
+  useEffect(() => {
+    console.log('useEffect - citas');
+  }, [citas]);
 
   //Crear cita
   const crearCita = (citaDelFormulario) => {
@@ -17,15 +22,20 @@ function App() {
     setCitas(citas.filter((cita) => cita.id !== id));
   };
 
+  //Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Citas agendadas';
+
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
       <div className='container'>
         <div className='row'>
           <div className='one-half column'>
+            <h1>Crear cita</h1>
             <Formulario crearCita={crearCita} />
           </div>
           <div className='one-half column'>
+            <h1>{titulo}</h1>
             {citas.map((cita) => (
               <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
             ))}
